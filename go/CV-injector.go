880175a740b3c78869f54011ac099929ee9b784a
Lajule <julien.rouzieres@mac.com>
2016-09-03 01:53:12 +0200

    update

--- go/CV-injector.go
+++ go/CV-injector.go
@@ -1,10 +1,47 @@
 package main
 
 import (
-	"fmt"
-  "time"
+	"bufio"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
+func parse(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	html := string(bytes)
+	log.Println(html)
+}
+
 func main() {
-  fmt.Printf("CV-injector %v\nCopy/Paste LinkedIn public profile url...\n", time.Now())
+	log.Println("CV-injector")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		log.Println("Copy/Paste LinkedIn public profile url...")
+		url, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		url = strings.TrimSuffix(url, "\n")
+		log.Printf("Inject profile %v\n", url)
+		parse(url)
+	}
 }
