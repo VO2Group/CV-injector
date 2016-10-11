9fa592bfeceff9eb426805f9d5f565371f72ec01
Lajule <julien.rouzieres@mac.com>
2016-10-11 11:46:13 +0200

    add fucking linkedin skills

--- injector.js
+++ injector.js
@@ -3,51 +3,19 @@ var page = require('webpage').create()
 
 page.open(system.args[1], function (status) {
   if ('success' === status) {
-    var name = page.evaluate(function () {
-      return document.getElementById('name')
+    var r = page.evaluate(function () {
+      return {
+        name: document.getElementById('name').textContent,
+        headline: document.querySelector('#topcard > div.profile-card.vcard > div.profile-overview > div > p').textContent,
+        locality: document.querySelector('#demographics > dd.descriptor.adr > span').textContent,
+        domain: document.querySelector('#demographics > dd:nth-child(4)').textContent,
+        firm: document.querySelector('#topcard > div.profile-card.vcard > div.profile-overview > div > table > tbody > tr:nth-child(1) > td > ol').textContent,
+        firms: document.querySelector('#topcard > div.profile-card.vcard > div.profile-overview > div > table > tbody > tr:nth-child(2) > td > ol').textContent.split(', '),
+        educations: document.querySelector('#topcard > div.profile-card.vcard > div.profile-overview > div > table > tbody > tr:nth-child(3) > td > ol').textContent,
+        skills: [].slice.call(document.querySelectorAll('.skill')).map(function (s) { return s.textContent })
+      }
     })
-    console.log(name.textContent)
-
-    var headline = page.evaluate(function () {
-      return document.querySelector('#topcard > div.profile-card.vcard > div.profile-overview > div > p')
-    })
-    console.log(headline.textContent)
-
-    var locality = page.evaluate(function () {
-      return document.querySelector('#demographics > dd.descriptor.adr > span')
-    })
-    console.log(locality.textContent)
-
-    var domain = page.evaluate(function () {
-      return document.querySelector('#demographics > dd:nth-child(4)')
-    })
-    console.log(domain.textContent)
-
-    var firm = page.evaluate(function () {
-      return document.querySelector('#topcard > div.profile-card.vcard > div.profile-overview > div > table > tbody > tr:nth-child(1) > td > ol > li > span > a')
-    })
-    console.log(firm.textContent)
-    console.log(firm.href)
-
-    var firms = page.evaluate(function () {
-      return document.querySelectorAll('#topcard > div.profile-card.vcard > div.profile-overview > div > table > tbody > tr:nth-child(2) > td > ol > li')
-    })
-    for (i = 1; i <= firms.length; i++) {
-      var f = page.evaluate(function (i) {
-        return document.querySelector('#topcard > div.profile-card.vcard > div.profile-overview > div > table > tbody > tr:nth-child(2) > td > ol > li:nth-child(' + i + ')')
-      }, i)
-      console.log(f.textContent)
-      console.log(f.tagName)
-    }
-
-    // [].slice.call(page.evaluate(function () {
-    //   return document.querySelectorAll('#topcard > div.profile-card.vcard > div.profile-overview > div > table > tbody > tr:nth-child(2) > td > ol > li > a')
-    // }))
-    // .forEach(function (f) {
-    //   console.log(f.textContent)
-    //   console.log(f.href)
-    // })
-
+    console.log(JSON.stringify(r))
   }
   phantom.exit(0)
 })
