84c9102fe5cac3fd00fb0d27e0a622ec8f432666
Lajule <julien.rouzieres@mac.com>
2016-10-23 13:49:58 +0200

    phantomjs is not the good thing to do the job

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-var child_process = require('child_process')
-    Botkit = require('botkit'),
+var Botkit = require('botkit'),
+    linkedinScraper = require('linkedin-scraper2'),
     jsforce = require('jsforce')
 
 var config = require(process.argv[2])
@@ -17,59 +17,84 @@ controller.hears(['^bonjour$', '^salut$', '^hello$'], 'direct_message,direct_men
 })
 
 controller.hears(['contact <https://fr.linkedin.com/in/(.*)>'], 'direct_message,direct_mention,mention', function (bot, message) {
-  var data = '',
-      url = 'https://fr.linkedin.com/in/' + message.match[1],
-      scraper = child_process.spawn(__dirname + '/node_modules/.bin/phantomjs', [
-        __dirname + '/scraper.js',
-        url
-      ])
-  scraper.stdout.on('data', function (buffer) {
-    data += buffer.toString()
-  })
-  scraper.on('close', function (code) {
-    bot.startConversation(message, function (err, convo) {
-      convo.ask("Vous allez créer ou mettre à jour un contact dans Salesforce, à partir de l'url: " + url + ', on continue ?', [
-        {
-          pattern: 'oui',
-          callback: function (response, convo) {
-            convo.say('ok on continue, je contacte Salesforce...')
-            connection.login(config.username, config.password + config.securitytoken, function (err, res) {
-              if (err) {
-                convo.say("Salesforce à l'air occupé, je n'arrive pas à le contacter, rappellez moi dans quelques minutes")
-                convo.say('Contactez le support IT si le problème persite')
-                convo.next()
-              }
-              else {
-                try {
-                  var contact = JSON.parse(data)
-                  connection.query("SELECT Id, Name, Email FROM Contact WHERE Name = '" + contact.name + "'", function (err, res) {
+  var url = 'https://fr.linkedin.com/in/' + message.match[1]
+
+  linkedinScraper(url, function (err, profile) {
+    if (err) {
+      bot.reply(message, "linkedIn trouve qu'on le solicite trop, rappellez moi dans quelques minutes")
+      bot.reply(message, 'Contactez le support IT si le problème persite')
+    } else {
+      bot.startConversation(message, function (err, convo) {
+        convo.ask("Vous allez créer ou mettre à jour un contact dans Salesforce, à partir de l'url: " + url + ', on continue ?', [
+          {
+            pattern: 'oui',
+            callback: function (response, convo) {
+              convo.say('ok on continue, je contacte Salesforce...')
+              connection.login(config.username, config.password + config.securitytoken, function (err, res) {
+                if (err) {
+                  convo.say("Salesforce à l'air occupé, je n'arrive pas à le contacter, rappellez moi dans quelques minutes")
+                  convo.say('Contactez le support IT si le problème persite')
+                  convo.next()
+                }
+                else {
+                  connection.query("SELECT Id, Name, Email FROM Contact WHERE Name = '" + profile.name + "'", function (err, res) {
                     if (err) {
                       convo.say('Salesforce ne me répond pas, contactez le support IT')
                       convo.next()
                     }
                     else {
-                      convo.say(JSON.stringify(res))
+                      if (res.totalSize != 1) {
+                        convo.ask('Salesforce ne connait pas ' + profile.name + ', voulez vous créer le contact ?', [
+                          {
+                            pattern: 'oui',
+                            callback: function (response, convo) {
+                              convo.say('insert')
+                              convo.next()
+                            }
+                          },
+                          {
+                            default: true,
+                            callback: function (response, convo) {
+                              convo.say("on s'arrête là alors!")
+                              convo.next()
+                            }
+                          }
+                        ])
+                      }
+                      else {
+                        convo.ask('Salesforce connait déjà ' + profile.name + ', voulez vous mettre à jour le contact ?', [
+                          {
+                            pattern: 'oui',
+                            callback: function (response, convo) {
+                              convo.say('update')
+                              convo.next()
+                            }
+                          },
+                          {
+                            default: true,
+                            callback: function (response, convo) {
+                              convo.say("on s'arrête là alors!")
+                              convo.next()
+                            }
+                          }
+                        ])
+                      }
                       convo.next()
                     }
                   })
                 }
-                catch(err) {
-                  convo.say("linkedIn trouve qu'on le solicite trop, rappellez moi dans quelques minutes")
-                  convo.say('Contactez le support IT si le problème persite')
-                  convo.next()
-                }
-              }
-            })
+              })
+            }
+          },
+          {
+            default: true,
+            callback: function (response, convo) {
+              convo.say("on s'arrête là alors!")
+              convo.next()
+            }
           }
-        },
-        {
-          default: true,
-          callback: function (response, convo) {
-            convo.say("on s'arrête là alors!")
-            convo.next()
-          }
-        }
-      ])
-    })
+        ])
+      })
+    }
   })
 })
