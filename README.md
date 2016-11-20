763f453d63409b81fdb0b8923564d488e5b58b15
Lajule <julien.rouzieres@mac.com>
2016-11-20 22:11:39 +0100

    es6 version

--- README.md
+++ README.md
@@ -1,11 +1,42 @@
 # CV-injector
 
-A Slack bot to play with Salesforce.
+A Slack bot to play with Salesforce (made with [Botkit][1]).
 
-# Utilisation
+# Usage
 
 ```
-token=<bot token> node . ./config.json
+TOKEN=<bot token> npm start -- ../config.json
+```
+
+## Configuration
+
+in **./config.json** :
+
+```json
+{
+  "version": "<version>",
+  "clientid": "<client id>",
+  "clientsecret": "<client secret>",
+  "username": "<email>",
+  "password": "<password>",
+  "securitytoken": "<security token>",
+  "environment": "<environment>"
+}
+```
+
+> The Salesforce user must be able to create or modify records in Contact object !
+
+## Salesforce
+
+This bot uses [JSforce][2] library to perform actions in your sweet Salesforce.
+
+## Run as daemon
+
+Use this command to launch your bot eternally:
+
+```
+nohup TOKEN=<bot token> npm start -- ../config.json >./bot.log 2>&1 &
 ```
 
 [1]: https://howdy.ai/botkit/ "Botkit"
+[2]: https://jsforce.github.io/ "JSforce"
--- index.js
+++ index.js
@@ -1,121 +1,52 @@
 import { readFileSync } from 'fs'
 
 import Botkit from 'botkit'
-import jsforce from 'jsforce'
-import linkedinScraper from 'linkedin-scraper2'
 
-const config = readFileSync(process.argv[2])
+import linkedIn from './lib/linkedin.js'
+import { login, query } from './lib/salesforce.js'
+import { createConvo, updateConvo } from './lib/convo.js'
+
+const config = JSON.parse(readFileSync(process.argv[2]))
+const token =  process.env.TOKEN
+
 const controller = Botkit.slackbot()
-const connection = new jsforce.Connection()
+const bot = controller.spawn({ token }).startRTM()
 
-const bot = controller.spawn({
-  token: process.env.token
-}).startRTM()
+const where = ['direct_message', 'direct_mention', 'mention']
 
-function stopHere(response, convo) {
-  convo.say("Ok, on s'arrête là alors!")
-  convo.next()
-}
-
-function insertContact(profile) {
-  return function (response, convo) {
-    connection
-      .sobject('Contact')
-      .create({
-        Name: profile.name
-      },
-      function (err, res) {
-        convo.say('insert: ' + JSON.stringify(err || {}))
-        convo.say('insert: ' + JSON.stringify(res || {}))
-        convo.next()
-      })
-  }
-}
-
-function updateContact(profile, contact) {
-  return function (response, convo) {
-    console.log(contact)
-    connection
-      .sobject('Contact')
-      .update({
-        Id: contact.Id,
-        Name: profile.name
-      },
-      function (err, res) {
-        convo.say('update: ' + JSON.stringify(err || {}))
-        convo.say('update: ' + JSON.stringify(res || {}))
-        convo.next()
-      })
-  }
-}
-
-controller.hears(['bonjour', 'salut', 'hello', 'coucou', 'cc'], 'direct_message,direct_mention,mention', function (bot, message) {
+controller.hears(['hello', 'hi', 'bonjour', 'salut', 'coucou', 'cc'], where, (bot, message) => {
   bot.reply(message, 'Bonjour, je suis un spécialiste Salesforce! Que puis je faire pour vous ?')
 })
 
-controller.hears(['contact <https://fr.linkedin.com/in/(.*)>'], 'direct_message,direct_mention,mention', function (bot, message) {
-  var url = 'https://fr.linkedin.com/in/' + message.match[1]
-
-  linkedinScraper(url, function (err, profile) {
-    if (err) {
-      bot.reply(message, 'LinkedIn ne me répond pas (' + JSON.stringify(err) + '), contactez le support IT')
-    }
-    else {
-      bot.startConversation(message, function (err, convo) {
-        convo.ask("Vous allez créer ou mettre à jour un contact dans Salesforce, à partir de l'url: " + url + ', on continue ?', [
-          {
-            pattern: 'oui',
-            callback: function (response, convo) {
-              convo.say('ok on continue, je contacte Salesforce...')
-              connection.login(config.username, config.password + config.securitytoken, function (err, res) {
-                if (err) {
-                  convo.say('Salesforce ne me répond pas (' + JSON.stringify(err) + '), contactez le support IT')
-                  convo.next()
-                }
-                else {
-                  connection.query("SELECT ID,ISDELETED,MASTERRECORDID,ACCOUNTID,LASTNAME,FIRSTNAME,SALUTATION,MIDDLENAME,SUFFIX,NAME,RECORDTYPEID,MAILINGSTREET,MAILINGCITY,MAILINGSTATE,MAILINGPOSTALCODE,MAILINGCOUNTRY,MAILINGLATITUDE,MAILINGLONGITUDE,MAILINGGEOCODEACCURACY,MAILINGADDRESS,PHONE,FAX,MOBILEPHONE,REPORTSTOID,EMAIL,TITLE,DEPARTMENT,DESCRIPTION,CURRENCYISOCODE,OWNERID,CREATEDDATE,CREATEDBYID,LASTMODIFIEDDATE,LASTMODIFIEDBYID,SYSTEMMODSTAMP,LASTACTIVITYDATE,LASTCUREQUESTDATE,LASTCUUPDATEDATE,LASTVIEWEDDATE,LASTREFERENCEDDATE,EMAILBOUNCEDREASON,EMAILBOUNCEDDATE,ISEMAILBOUNCED,PHOTOURL,JIGSAWCONTACTID,RELATIONSHIP_LEVEL__C,NEWSLETTER__C,CONTACT_ROLE__C,CREATE_A_RESOURCE__C,TYPE_OF_RESOURCE__C,DATE_OF_BIRTH__C,N_S_CURIT_SOCIALE_FRANCE__C,NATIONALITY__C,OFFER__C,PLACE_OF_BIRH__C,PROFESSIONAL_EMAIL__C,PROFILE_LEVEL__C,ROLE__C,TECHNO__C,UNAVAILABILITY_STARTING_DATE__C,USERRELATED__C,AVAILABILITY__C,LINKEDIN_ET_AUTRES_SITES__C,EXPECTATION__C,EXPECTATION_TEXT__C,SOURCING_PRINCIPAL__C,OBSERVATIONS__C,GODFATHERMOTHER__C,SOURCING_SECONDAIRE__C,LIEU_DE_TRAVAIL__C FROM Contact WHERE Name = '" + profile.name + "'", function (err, res) {
-                    if (err) {
-                      convo.say('Salesforce ne me répond pas (' + JSON.stringify(err) + '), contactez le support IT')
-                      convo.next()
-                    }
-                    else {
-                      if (res.totalSize == 0) {
-                        convo.ask('Salesforce ne connait pas ' + profile.name + ', voulez vous créer le contact ?', [
-                          {
-                            pattern: 'oui',
-                            callback: insertContact(profile)
-                          },
-                          {
-                            default: true,
-                            callback: stopHere
-                          }
-                        ])
-                      }
-                      else {
-                        convo.ask('Salesforce connait déjà ' + profile.name + ', voulez vous mettre à jour le contact ?', [
-                          {
-                            pattern: 'oui',
-                            callback: updateContact(profile, res.records[0])
-                          },
-                          {
-                            default: true,
-                            callback: stopHere
-                          }
-                        ])
-                      }
-                      convo.next()
-                    }
-                  })
-                }
-              })
-            }
-          },
-          {
-            default: true,
-            callback: stopHere
-          }
-        ])
-      })
-    }
-  })
+controller.hears(['<https://fr.linkedin.com/in/(.*)>'], where, (bot, message) => {
+  const url = `https://fr.linkedin.com/in/${message.match[1]}`
+  console.log(url)
+  linkedIn(url)
+    .then((profile) => {
+      console.log(profile)
+      login(config)
+        .then(() => {
+          query(profile)
+            .then((contacts) => {
+              console.log(contacts)
+              if (contacts.totalSize == 0) {
+                createConvo(bot, message, url, profile)
+              }
+              else {
+                const contact = contacts.records[0]
+                console.log(contact)
+                updateConvo(bot, message, url, contact, profile)
+              }
+            })
+            .catch((err) => {
+              bot.reply(message, `Contactez le support IT (query, ${err})`)
+            })
+        })
+        .catch((err) => {
+          bot.reply(message, `Contactez le support IT (login, ${err})`)
+        })
+    })
+    .catch((err) => {
+      bot.reply(message, `Contactez le support IT (linkedIn, ${err})`)
+    })
 })
